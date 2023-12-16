e550c96c4b04299ded1135d312378e4845efbed0
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-12-16 00:20:07 -0500

    js-tdd-writing-unit-tests/
    js-tdd-writing-unit-tests/

--- tutils.test.js
+++ tutils.test.js
@@ -1,14 +1,4 @@
-export function pointsForWord(word) {
-  let points = 0;
-  for (const char of word) {
-    points += /[aeiou]/i.test(char) ? 1 : 2;
-  }
-  return points;
-}
-it("handles uppercase and lowercase input", () => {
-  const word = "tEsT";
-
-  const points = pointsForWord(word);
-
-  expect(points).toBe(7);
+test('example test', () => {
+  // Your test code goes here
+  expect(2 + 2).toBe(4);
 });
